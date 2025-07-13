b356192a75b968dedd349fa87b42f3b5d934923a
Davilog <61393129+Davilog@users.noreply.github.com>
2025-07-13 14:31:54 +0100

    Create 2-arguments.js

---
+++ 2-arguments.js
@@ -0,0 +1,10 @@
+const args = process.argv.slice(2);
+if (args.length === 0) {
+    console.log('No arguments provided');
+}
+else if (args.length === 1) {
+    console.log(`Argument provided: ${args[0]}`);
+}
+else {
+    console.log(`Arguments provided: ${args.join(', ')}`);
+}
