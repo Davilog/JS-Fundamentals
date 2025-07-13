4d8abd6230565286d6fb1a72217c6aa7e7651769
Davilog <61393129+Davilog@users.noreply.github.com>
2025-07-13 14:36:29 +0100

    Update 2-arguments.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,8 +1,8 @@
 const args = process.argv.slice(2);
-if (args.length === 0) {
+if (args.test === 0) {
     console.log('No arguments provided');
 }
-else if (args.length === 1) {
+else if (args.test === 1) {
     console.log(`Argument provided: ${args[0]}`);
 }
 else {
