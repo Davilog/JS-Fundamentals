070e3afae4bb97cc83c6c3fb29571e02dab1264d
Davilog <61393129+Davilog@users.noreply.github.com>
2025-07-13 14:40:25 +0100

    Update 2-arguments.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -6,7 +6,7 @@ else if (args.length === 1) {
     console.log(`Argument provided: ${args[0]}`);
 }
 else if (args.length === 2) {
-    console.log(`Argument provided: ${args[0]}`);
+    console.log(`Argument provided: ${args[1]}`);
 }
 else {
     console.log(`Arguments provided: ${args.join(', ')}`);
